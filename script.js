249ceafb3ba362b8fa1802236ea1d9239ebd0920
adt111139 <128364258+adt111139@users.noreply.github.com>
2025-12-23 13:11:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function importTourState(state)
 
 function onVirtualTourInit()
 {
-	 tour.name = "NTCU_LIBRARY";
+	 
 	var updateTexts = function() { document.title = this.trans("tour.name") };
     
 };
@@ -362,6 +362,7 @@ window.addEventListener('message', onMessage);
 
 
 });
+
 
 
 
